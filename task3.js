f512fa35f8e1abc5a0ad4e544acae75b1968792f
Pholisa <pholisa@projectcodex.co>
2015-02-18 08:52:16 +0200

    task 3 is done

---
+++ task3.js
@@ -0,0 +1,7 @@
+function ChangeColor() {
+	document.getElementById("sqr").style.backgroundColor = "green";
+};
+
+function ChangeColor1() {
+	document.getElementById("sqr").style.backgroundColor = "red";
+};
