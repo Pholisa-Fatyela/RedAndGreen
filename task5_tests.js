5deaa7549707840ba0568d068847f726fc0f04b4
Pholisa <pholisa@projectcodex.co>
2015-02-19 12:11:32 +0200

    task5 is done

---
+++ task5_tests.js
@@ -0,0 +1,10 @@
+(function() {
+setBackgroundColor("mySquare",'red');
+	
+		if(document.getElementById("mySquare").style.backgroundColor === "red"){
+			window.alert("success!");
+		}
+		else{
+			window.alert("failure");
+		}
+}) ();
