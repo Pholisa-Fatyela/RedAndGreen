06cbbc7468740caaeaf4db424640f727da76cdb1
Pholisa <pholisa@projectcodex.co>
2015-02-27 11:29:09 +0200

    modified

--- tdd.js
+++ tdd.js
@@ -2,32 +2,37 @@ var assert = {
 	i:0,
 	pass:0,
 
-	equals: function(message,result) {
+	equals: function(message,result,newmsg) {
+		//creating a new element
 		var newBlock = document.createElement('div');
 
+		//Giving the newly created div style
 		newBlock.id = "square"+this.i;
 		newBlock.style.height = "150px";
 		newBlock.style.width = "150px";
-		newBlock.style.border = "5px solid black";
+		newBlock.style.border = "solid black 1px";
 		newBlock.style.display = "inline-Block";
 
+		//Appending the new div you have created to the body
 		document.body.appendChild(newBlock);
 
+		//Checking if the square is green or red
 		var rg = new RedOrGreen(newBlock.id);
 		if(message === result) {
-			newBlock.innerHTML= "Pass";
+			newBlock.innerHTML= newmsg;
 			rg.makeGreen();
+			//incrementing pass for the results so we can see how many tests have passed
 			this.pass++;
 
 		}
 		else{
-			newBlock.innerHTML= "Fail";
+			newBlock.innerHTML= newmsg;
 			rg.makeRed();
 		}
 		this.i++;
 	}
 }
-
+//
 var TestMyCode = {
 	run: function(name, assertTest){
 		this.name = name;
@@ -35,10 +40,12 @@ var TestMyCode = {
 	}
 }
 
-
+//new function for storing the results
 function results() {
+	//creating a new p element
 	var dee = document.createElement('p');
-
+	//Appending the p element to the body of the code
 	document.body.appendChild(dee);
+	//setting the innerHtml for dee to the specified text
 	dee.innerHTML = "Number of passed tests is " + assert.pass + " out of " + assert.i;
 }
--- tdd_tests.js
+++ tdd_tests.js
@@ -1,25 +1,25 @@
 TestMyCode.run("testing hello world function", function(assert){
     var result = helloWorld();
     // is the result as we expected?
-    assert.equals("hello world!", result);
+    assert.equals("hello world!", result, "testing the helloWorld function ");
 });
 
 TestMyCode.run("testing bark function", function(assert){
     var result = bark();
     // is the result as we expected?
-    assert.equals("hello world!", result);
+    assert.equals("hello world!", result,"testing the bark function ");
 });
 
 TestMyCode.run("testing blah function", function(assert){
     var result = blah();
     // is the result as we expected?
-    assert.equals("blah blah blah blah!", result);
+    assert.equals("blah blah blah blah!", result, "testing the blah function ");
 });
 
 TestMyCode.run("testing mama function", function(assert){
     var result = mama();
     // is the result as we expected?
-    assert.equals("blah blah blah blah!", result);
+    assert.equals("blah blah blah blah!", result, "testing the mama function");
 });
 
 results();
